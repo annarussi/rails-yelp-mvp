666755bfc2666f7057d85eef910a467310ceb52a
Anna Russi <annarussi@gmail.com>
2023-05-24 14:55:05 +0100

    exercise done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+CATEGORIES = %w[chinese italian japanese french belgian].freeze
+
+puts 'Deleting existing DB'
+Restaurant.destroy_all
+Review.destroy_all
+
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.city,
+    phone_number: Faker::PhoneNumber,
+    category: CATEGORIES.sample
+  )
+  puts "creating #{restaurant.name}"
+end
